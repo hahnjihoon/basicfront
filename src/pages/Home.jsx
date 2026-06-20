afe3ac1c60086b8409de35324fb33ce4245ddaa3
jihoon hahn <hahnjh0426@naver.com>
2026-06-21 08:53:16 +0900

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import '../css/home.css';
+
+export default function Home() {
+  return (
+    <main className="home">
+      <section className="home__panel">
+        <p className="home__label">React Frontend</p>
+        <h1 className="home__title">basicfront</h1>
+        <p className="home__description">기본 React 프로젝트가 준비되었습니다.</p>
+      </section>
+    </main>
+  );
+}
